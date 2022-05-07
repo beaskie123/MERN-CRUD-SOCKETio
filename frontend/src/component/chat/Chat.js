4d62828c176f42f5254d8b7acaf9d09c4e4c6ca0
bea andrea <petermagalong0421@gmail.com>
2022-05-06 23:32:59 -0700

    second commit

--- frontend/src/component/chat/Chat.js
+++ frontend/src/component/chat/Chat.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
 import ScrollToBottom from "react-scroll-to-bottom";
+import { Container,  Nav,  Navbar, NavDropdown } from "react-bootstrap";
 
 function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
@@ -31,48 +32,90 @@ function Chat({ socket, username, room }) {
   }, [socket]);
 
   return (
-    <div className="chat-window">
-      <div className="chat-header">
-        <p>Live Chat</p>
-      </div>
-      <div className="chat-body">
-        <ScrollToBottom className="message-container">
-          {messageList.map((messageContent,index) => {
-            return (
-              <div
-                className="message"
-                id={username === messageContent.author ? "you" : "other"}
-                key={index}
-              >
-                <div>
-                  <div className="message-content">
-                    <p>{messageContent.message}</p>
+    <Container className=' container'>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand>Babble Space</Navbar.Brand> 
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+          {/* <Form className="joinChatContainer d-flex">
+              <FormControl
+                type="text"
+                placeholder="Enter your NickName"
+                className="me-2"
+                onChange={(event) => {
+                  setUsername(event.target.value);
+                }}
+              />
+          </Form>
+          <Form className="joinChatContainer d-flex">
+              <FormControl
+                 type="text"
+                 placeholder="Enter Room ID..."
+                className="me-2"
+                onChange={(event) => {
+                  setRoom(event.target.value);
+                }}
+              />
+          </Form>
+            <Form className="joinChatContainer d-flex">
+          <button onClick={joinRoom}>Join A Room</button>
+          </Form> */}
+            <Nav className="me-auto">
+              <NavDropdown title="Your Name Here" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className="App">
+        <div className="chat-window">
+          <div className="chat-header">
+            <p>Live Chat</p>
+          </div>
+          <div className="chat-body">
+            <ScrollToBottom className="message-container">
+              {messageList.map((messageContent,index) => {
+                return (
+                  <div
+                    className="message"
+                    id={username === messageContent.author ? "you" : "other"}
+                    key={index}
+                  >
+                    <div>
+                      <div className="message-content">
+                        <p>{messageContent.message}</p>
+                      </div>
+                      <div className="message-meta">
+                        <p id="time">{messageContent.time}</p>
+                        <p id="author">{messageContent.author}</p>
+                      </div>
+                    </div>
                   </div>
-                  <div className="message-meta">
-                    <p id="time">{messageContent.time}</p>
-                    <p id="author">{messageContent.author}</p>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </ScrollToBottom>
-      </div>
-      <div className="chat-footer">
-        <input
-          type="text"
-          value={currentMessage}
-          placeholder="Hey..."
-          onChange={(event) => {
-            setCurrentMessage(event.target.value);
-          }}
-          onKeyPress={(event) => {
-            event.key === "Enter" && sendMessage();
-          }}
-        />
-        <button onClick={sendMessage}>&#9658;</button>
-      </div>
+                );
+              })}
+            </ScrollToBottom>
+          </div>
+          <div className="chat-footer">
+            <input
+              type="text"
+              value={currentMessage}
+              placeholder="Hey..."
+              onChange={(event) => {
+                setCurrentMessage(event.target.value);
+              }}
+              onKeyPress={(event) => {
+                event.key === "Enter" && sendMessage();
+              }}
+            />
+            <button onClick={sendMessage}>&#9658;</button>
+          </div>
+        </div>
     </div>
+    </Container>
   );
 }
 
--- frontend/src/screen/Main.js
+++ frontend/src/screen/Main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { Container, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "../component/chat/Chat";
@@ -20,16 +20,39 @@ function Main() {
 
   return (
     <div className='main-container'>
-      <Container className='container'>
+     {!showChat ? (
+      <Container className=' container'>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand>Babble Space</Navbar.Brand>
+          <Navbar.Brand>Babble Space</Navbar.Brand> 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
+          <Form className="joinChatContainer d-flex">
+              <FormControl
+                type="text"
+                placeholder="Enter your NickName"
+                className="me-2"
+                onChange={(event) => {
+                  setUsername(event.target.value);
+                }}
+              />
+          </Form>
+          <Form className="joinChatContainer d-flex">
+              <FormControl
+                 type="text"
+                 placeholder="Enter Room ID..."
+                className="me-2"
+                onChange={(event) => {
+                  setRoom(event.target.value);
+                }}
+              />
+          </Form>
+            <Form className="joinChatContainer d-flex">
+          <button onClick={joinRoom}>Join A Room</button>
+          </Form>
             <Nav className="me-auto">
               <NavDropdown title="Your Name Here" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
-  
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
               </NavDropdown>
@@ -38,30 +61,12 @@ function Main() {
         </Container>
       </Navbar>
       <div className="App">
-      {!showChat ? (
-        <div className="joinChatContainer">
-          <h3>Join A Chat</h3>
-          <input
-            type="text"
-            placeholder="John..."
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
-          />
-          <input
-            type="text"
-            placeholder="Room ID..."
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-          />
-          <button onClick={joinRoom}>Join A Room</button>
-        </div>
+        <h1>Join a room now !</h1>
+    </div>
+      </Container>
       ) : (
         <Chat socket={socket} username={username} room={room} />
       )}
-    </div>
-      </Container>
     </div>
   )
 }
